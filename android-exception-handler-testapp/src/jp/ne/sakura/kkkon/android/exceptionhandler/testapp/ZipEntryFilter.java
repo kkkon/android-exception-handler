1f36119a3441ff6e158bb447f63e755168e11599
kkkon <kiyofumi.kondoh+github@gmail.com>
2014-08-09 10:55:36 +0900

    change to fullname

--- android-exception-handler-testapp/src/jp/ne/sakura/kkkon/android/exceptionhandler/testapp/ZipEntryFilter.java
+++ android-exception-handler-testapp/src/jp/ne/sakura/kkkon/android/exceptionhandler/testapp/ZipEntryFilter.java
@@ -1,7 +1,7 @@
 /*
  * The MIT License
  *
- * Copyright 2014 kkkon.
+ * Copyright 2014 Kiyofumi Kondoh
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -27,7 +27,7 @@ import java.util.zip.ZipEntry;
 
 /**
  *
- * @author kkkon
+ * @author Kiyofumi Kondoh
  */
 public interface ZipEntryFilter
 {
